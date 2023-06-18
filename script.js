9040b17ca0f1964cdf28ff61839467825a6dcfa4
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-06-19 01:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// your code here
+ document.getElementById("urlForm").addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      // Get form values
+      var nameValue = document.getElementById("name").value;
+      var yearValue = document.getElementById("year").value;
+
+      // Generate query string
+      var queryString = "name=" + encodeURIComponent(nameValue) + "&year=" + encodeURIComponent(yearValue);
+
+      // Update URL
+      var urlElement = document.getElementById("url");
+      urlElement.textContent = "https://localhost:8080/?" + queryString;
+    });
